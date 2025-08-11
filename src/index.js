1523999c03061c0ec22ad88691fa8cad3b1ce154
PandesalPanpan <petermarticio@gmail.com>
2025-08-11 17:35:00 +0800

    Feat: Add Project class that can contain todos

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
 import './styles.css';
 import Todo from './todos.js';
+import Project from './projects.js';
 
 const test = new Todo('Old Title', 'Description', 'dueDate', 'priority');
+const test2 = new Todo('Test 2', 'Sample Description', 'what Date', 'priority');
+
+const project = new Project('Project Oblak', [test, test2]);
+
+console.log(project);
+console.log(project.todos[0].title)
