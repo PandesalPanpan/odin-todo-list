5e1c451742f643c315cdfc5b578363f4b731f113
PandesalPanpan <petermarticio@gmail.com>
2025-08-11 17:45:11 +0800

    Feat: Add skeleton htmlt o select for index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import './styles.css';
 import Todo from './todos.js';
 import Project from './projects.js';
 
+const content = document.querySelector('#content');
+content.textContent = 'This is the content';
+
 const test = new Todo('Old Title', 'Description', 'dueDate', 'priority');
 const test2 = new Todo('Test 2', 'Sample Description', 'what Date', 'priority');
 
@@ -9,3 +12,6 @@ const project = new Project('Project Oblak', [test, test2]);
 
 console.log(project);
 console.log(project.todos[0].title)
+
+// Create a function that grabs all todos in
+// all existing projects and put them in a single project
