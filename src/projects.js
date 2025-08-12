f1b779d56ca19e0365831c93188e4ae746671498
PandesalPanpan <petermarticio@gmail.com>
2025-08-12 11:25:34 +0800

    Feat: Track all instances of projects with static methods and constructor calling it

--- src/projects.js
+++ src/projects.js
@@ -1,6 +1,14 @@
 export default class Project {
+    static projects = [];
+
+    static getAllProjects = () => {
+        return Project.projects;
+    }
+
     constructor(name, todos) {
         this.name = name;
         this.todos = todos;
+
+        Project.projects.push(this);
     }
 }
