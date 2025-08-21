7de3e568736e79f79e817684bd30769d8deac70d
PandesalPanpan <petermarticio@gmail.com>
2025-08-21 17:04:24 +0800

    Refactor: Projects now use localStorage to globally track projects

--- src/projects.js
+++ src/projects.js
@@ -1,25 +1,74 @@
 import Todo from "./todos";
 
 export default class Project {
-    static projects = [];
+    static fromJSON = (data) => {
+        const project = new Project(data.name, true);
+        project.uuid = data.uuid;
+        project.todos = data.todos.map(todoData => {
+            const todo = new Todo(
+                todoData.title,
+                todoData.description,
+                todoData.dueDate,
+                todoData.priority
+            )
+            return todo;
+        })
+
+        return project;
+    }
 
     static getAllProjects = () => {
-        return Project.projects;
+        const data = JSON.parse(localStorage.getItem('odinTodoList') || '{"projects":[]}');
+        return data.projects.map(projectData => Project.fromJSON(projectData));
+    }
+
+    static saveAllProjects = (projects) => {
+        const data = {
+            projects: projects.map(project => project.toJSON())
+        };
+        localStorage.setItem('odinTodoList', JSON.stringify(data));
     }
 
     static findByUUID = (uuid) => {
-        return Project.projects.find(project => project.uuid === uuid);
+        return Project.getAllProjects().find(project => project.uuid === uuid);
     }
-
-    constructor(name) {
+    constructor(name, skipSave = false) {
         this.uuid = crypto.randomUUID();
         this.name = name;
         this.todos = [];
-        Project.projects.push(this);
+        if (!skipSave) this.save();
+    }
+
+    toJSON = () => {
+        return {
+            uuid: this.uuid,
+            name: this.name,
+            todos: this.todos.map(todo => ({
+                uuid: todo.uuid,
+                title: todo.title,
+                description: todo.description,
+                dueDate: todo.dueDate,
+                priority: todo.priority
+            }))
+        }
+    }
+
+    save = () => {
+        const allProjects = Project.getAllProjects();
+        const existingIndex = allProjects.findIndex(p => p.uuid === this.uuid);
+
+        if (existingIndex >= 0) {
+            allProjects[existingIndex] = this;
+        } else {
+            allProjects.push(this);
+        }
+
+        Project.saveAllProjects(allProjects);
     }
 
     addTodo = (todo) => {
         if (todo instanceof Todo === false) return;
         this.todos.push(todo);
+        this.save();
     }
 }
